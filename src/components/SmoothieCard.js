4cd6dcd7dfb249cfbe508140b16d3dfe54b27074
Kenny Sabangan <ksabangan121@gmail.com>
2024-05-21 03:42:17 -0700

    Finish Tutorial

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,9 +1,32 @@
-const SmoothieCard = ({ smoothie }) => {
+import { Link } from "react-router-dom";
+import supabase from "../config/supabaseClient";
+
+const SmoothieCard = ({ smoothie, reloadData }) => {
+  const handleDelete = async () => {
+    const { status } = await supabase
+      .from("smoothies")
+      .delete()
+      .eq("id", smoothie.id);
+
+    // Refresh the page
+    if (status === 204) {
+      reloadData();
+    }
+  };
+
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+        <i className="material-icons" onClick={handleDelete}>
+          delete
+        </i>
+      </div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,68 @@
 import supabase from "../config/supabaseClient";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import SmoothieCard from "../components/SmoothieCard";
 
 const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [smoothies, setSmoothies] = useState(null);
+  const [orderBy, setOrderBy] = useState("created_at");
+  const [isOrderAscending, setIsOrderAscending] = useState(false);
 
+  const fetchSmoothies = useCallback(async () => {
+    const { data, error } = await supabase
+      .from("smoothies")
+      .select()
+      .order(orderBy, { ascending: isOrderAscending });
+
+    if (error) {
+      setFetchError("Could not fetch the smoothies.");
+      setSmoothies(null);
+      console.log(error);
+    }
+
+    if (data) {
+      setSmoothies(data);
+      setFetchError(null);
+    }
+  }, [orderBy, isOrderAscending]);
+
+  // On load, fetch the smoothies data
   useEffect(() => {
-    const fetchSmoothies = async () => {
-      const { data, error } = await supabase.from("smoothies").select();
-
-      if (error) {
-        setFetchError("Could not fetch the smoothies.");
-        setSmoothies(null);
-        console.log(error);
-      }
-
-      if (data) {
-        setSmoothies(data);
-        setFetchError(null);
-      }
-    };
-
     fetchSmoothies();
-  }, []);
+  }, [orderBy, isOrderAscending, fetchSmoothies]);
 
   return (
     <div className="page home">
       {fetchError && <p>{fetchError}</p>}
       {smoothies && (
         <div className="smoothies">
-          {/* {Order by Buttons} */}
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy("title")}>Title</button>
+            <button onClick={() => setOrderBy("rating")}>Rating</button>
+            <button onClick={() => setOrderBy("created_at")}>
+              Time Created
+            </button>
+            <span>
+              <select
+                className="ascending-select"
+                onChange={(e) => {
+                  setIsOrderAscending(e.target.value === "true");
+                  console.log(e.target.value);
+                }}
+              >
+                <option value={false}>Descending</option>
+                <option value={true}>Ascending</option>
+              </select>
+            </span>
+          </div>
           <div className="smoothie-grid">
             {smoothies.map((smoothie) => (
-              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+              <SmoothieCard
+                key={smoothie.id}
+                smoothie={smoothie}
+                reloadData={fetchSmoothies}
+              />
             ))}
           </div>
         </div>
